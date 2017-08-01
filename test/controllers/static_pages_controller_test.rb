564e792982bc7bafb8969cca05162e47585983cf
soichirokando <skando@sonix.asia>
2017-08-01 18:41:57 +0900

    test of static_page responses

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,4 +4,23 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get home 2" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
