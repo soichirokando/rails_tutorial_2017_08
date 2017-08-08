b178924b41b98d8e90bad611dc1fbcf29486050c
soichirokando <skando@sonix.asia>
2017-08-08 16:28:23 +0900

    8.1.5 ログイン失敗時のフラッシュメッセージ表示まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
   get 'users/new'
 
   root 'static_pages#home'
@@ -8,8 +7,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  get  '/login',   to: 'session#new'
-  post '/login',   to: 'session#create'
-  delete '/logout',to: 'session#destroy'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',to: 'sessions#destroy'
   resource :users
 end
