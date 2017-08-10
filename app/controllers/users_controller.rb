faece727a8e00a0f6f969e80974dfbd0510ff992
soichirokando <skando@sonix.asia>
2017-08-10 16:59:52 +0900

    10.2.1 ユーザーのログイン確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only:[:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -26,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -37,5 +39,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
