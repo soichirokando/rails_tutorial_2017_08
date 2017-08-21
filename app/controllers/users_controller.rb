e0af4edbb71c1ecabf272fe74e2d1aa23c2ca146
soichirokando <skando@sonix.asia>
2017-08-21 11:54:32 +0900

    13.3.1まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,17 +56,7 @@ class UsersController < ApplicationController
     end
 
 
-    # beforeアクション
-
-    # ログイン済みユーザーかどうか確認
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # beforeフィルター
 
     # 正しいユーザーかどうか確認
     def correct_user
@@ -74,6 +64,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
